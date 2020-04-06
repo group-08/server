50cac757f145c7056120ce899050e748c3866e87
Oli Kamer <oliver@kamer.ch>
2020-04-06 12:04:56 +0200

    Add Board to JPA

--- src/main/java/ch/uzh/ifi/seal/soprafs20/board/Board.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/board/Board.java
@@ -1,16 +1,19 @@
 package ch.uzh.ifi.seal.soprafs20.board;
 
-
-
 import ch.uzh.ifi.seal.soprafs20.User.Player;
+import ch.uzh.ifi.seal.soprafs20.field.Field;
 
-import java.lang.reflect.Field;
+import java.io.Serializable;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public abstract class Board {
+public abstract class Board implements Serializable {
 
+    @OneToMany(targetEntity = Field.class)
     private Collection<Field> fields;
+
+    @OneToMany(targetEntity = Player.class)
     private ArrayList<Player> players;
 
     public void play(){
