e8baeef3f9cab4694f4da455a8ad5e726d2cde1d
Philip Flury <philip.flury@gmail.com>
2020-04-08 09:25:00 +0200

    added Player and Field parameter to Figure

--- src/main/java/ch/uzh/ifi/seal/soprafs20/User/Figure.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/User/Figure.java
@@ -1,11 +1,18 @@
 package ch.uzh.ifi.seal.soprafs20.User;
 
+import ch.uzh.ifi.seal.soprafs20.field.Field;
+
 public class Figure {
     Long id;
     Player player;
+    Field field;
 
 
     public long getId(){
         return this.id;
     }
+
+    public Player getPlayer() { return this.player; }
+
+    public Field getField() { return this.field; }
 }
