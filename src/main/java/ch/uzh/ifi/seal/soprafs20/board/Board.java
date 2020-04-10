880ebb26024ec3b2bd76fa6a2c7dc4a006024e53
Philip Flury <philip.flury@gmail.com>
2020-04-10 12:00:48 +0200

    removed redunand code

--- src/main/java/ch/uzh/ifi/seal/soprafs20/board/Board.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/board/Board.java
@@ -22,28 +22,12 @@ public abstract class Board {
     }
 
     /**
-     * Gets a currentField as well as a Card and returns the targetField
-     * @param currentField Current Field the figure is on
-     * @param card Card to be played
-     * @return the field the figure will land on
-     */
-    public Field getTargetField(Field currentField, Card card) {
-        /* takes value of card, adds it to current field and returns target field */
-    }
-
-    /**
      * Takes a Figure and gets Figures current Field
      * @param figure figure you want the current field of
      * @return the current field of the figure
      */
     public Field getCurrentField(Figure figure) {
-        /* loop through all the fields and check if occupant matches figure */
-        for (Field field: fields) {
-            if (field.getOccupant() == figure) {
-                return field;
-            }
-        }
-        return null;
+        return figure.getField();
     }
 
     /**
@@ -52,21 +36,6 @@ public abstract class Board {
      */
     public void sendFigureHome(Figure figure) {
         /* sends figure home */
-    }
-
-    /**
-     * This checks if there are any savezones involved in this move and if the move is possible
-     * @param figure figure to move
-     * @param targetField the field the figure wants to move on
-     * @return boolean if move is possible
-     */
-    public boolean isMovePossible(Figure figure, Field targetField) {
-        return false;
-    }
-
-    public void getPossibleTargetFields(Figure figure, Card card) {
-        Field currentField = this.getCurrentField(figure);
-        // gets all the possible moves from Adj. List, gets ID's of the fields and returns them as a list
     }
 
     public void move(Figure figure, Field targetField) {
