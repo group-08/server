4e1343870904c9ddc84a8694e43d1d9f39b5fc4f
Oli Kamer <oliver@kamer.ch>
2020-04-20 15:21:07 +0200

    Allows for the game to be created with all the players (FS20G08-40)

--- src/main/java/ch/uzh/ifi/seal/soprafs20/game/Game.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/game/Game.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.seal.soprafs20.game;
 
 import ch.uzh.ifi.seal.soprafs20.User.Player;
 import ch.uzh.ifi.seal.soprafs20.board.Board;
+import ch.uzh.ifi.seal.soprafs20.board.CasualBoard;
 import ch.uzh.ifi.seal.soprafs20.cards.Card;
 import ch.uzh.ifi.seal.soprafs20.cards.Deck;
 import ch.uzh.ifi.seal.soprafs20.field.Field;
@@ -11,6 +12,7 @@ import ch.uzh.ifi.seal.soprafs20.field.Field;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -39,21 +41,50 @@ public class Game implements Serializable {
     WeatherState weatherState;
 
     Game(){
-
+        this.board = new CasualBoard();
+        this.deck = new Deck();
+        this.gameState = GameState.PENDING;
     }
 
+    private boolean canAddPlayer() {
+        return this.players.size() <= 4; // Currently allow for games with 4 players
+    }
 
-    public void initialize(){
-
-
+    public void addPlayer(Player player) {
+        if (this.canAddPlayer()) {
+            this.players.add(player);
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException("Game is already full.");
+        }
     }
 
     public void startGame(){
+        // Mix up the players
+        Collections.shuffle(this.players);
 
+        // Shuffle the cards
+        this.deck.shuffle();
+
+        // Set the gameState to running
+        this.gameState = GameState.RUNNING;
     }
 
+    /**
+     * return the next player and keep moving
+     * (I understand this is basically a Queue, but I can't get JPA to accept a Queue)
+     * @return the next player
+     */
     public Player getNextPlayer(){
-        return null;
+        // Get player on top of the array
+        Player nextPlayer = this.players.get(0);
+
+        // Append the player at the back again
+        this.players.remove(nextPlayer);
+        this.players.add(nextPlayer);
+
+        // Return the player
+        return nextPlayer;
     }
 
     public void endGame(){
