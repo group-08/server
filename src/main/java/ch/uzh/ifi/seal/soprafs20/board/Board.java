183a605071c7263503d4ba956086ceb03bfc1b6c
Oli Kamer <oliver@kamer.ch>
2020-04-22 21:17:05 +0200

    #FS20G08-146 transfered most of the graph in the field class, had to break getPossibleFields in the progress

--- src/main/java/ch/uzh/ifi/seal/soprafs20/board/Board.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/board/Board.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.soprafs20.board;
 
 import ch.uzh.ifi.seal.soprafs20.User.Player;
+import ch.uzh.ifi.seal.soprafs20.cards.Value;
 import ch.uzh.ifi.seal.soprafs20.field.Field;
 
 import java.io.Serializable;
@@ -11,8 +12,9 @@ import ch.uzh.ifi.seal.soprafs20.cards.Card;
 import ch.uzh.ifi.seal.soprafs20.field.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 @Entity
 @Table(name = "BOARD")
@@ -30,21 +32,70 @@ public abstract class Board implements Serializable {
 
     protected int version;
 
-    @Transient
-    private Graph graphFields = new Graph();
-
     public Board() {
         // Create all the fields
         for (int i = 0; i < 96; i++){
             this.fields.add(new CasualField());
         }
 
-        // Add the graphs
-        this.graphFields.createGraph(this.fields);
+        // Add the graphs to all the field
+        this.addGraphs(this.fields);
+
     }
 
-    public Graph getGraph() {
-        return this.graphFields;
+    private void addGraphs(List<Field> fields) {
+        for(int id=0; id<63;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(id+1);
+            first.addAdjacency(second);
+        }
+
+        for(int id=64; id<67;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(id+1);
+            first.addAdjacency(second);
+        }
+        for(int id=68; id<71;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(id+1);
+            first.addAdjacency(second);
+        }
+        for(int id=72; id<75;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(id+1);
+            first.addAdjacency(second);
+        }
+        for(int id=76; id<79;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(id+1);
+            first.addAdjacency(second);
+        }
+        for(int id=80; id<84;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(0);
+            first.addAdjacency(second);
+        }
+        for(int id=84; id<88;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(16);
+            first.addAdjacency(second);
+        }
+        for(int id=88; id<92;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(32);
+            first.addAdjacency(second);
+        }
+        for(int id=92; id<96;id++){
+            Field first = fields.get(id);
+            Field second = fields.get(48);
+            first.addAdjacency(second);
+        }
+
+        fields.get(63).addAdjacency(fields.get(0));
+        fields.get(0).addAdjacency(fields.get(64));
+        fields.get(16).addAdjacency(fields.get(68));
+        fields.get(32).addAdjacency(fields.get(72));
+        fields.get(48).addAdjacency(fields.get(76));
     }
 
     /**
@@ -87,7 +138,64 @@ public abstract class Board implements Serializable {
     }
 
     public ArrayList<Field> getPossibleFields(Card card, Field field) {
-        return this.graphFields.getPossibleFields(card, field, graphFields);
+        int moveValue = card.getValue().getValue();
+        ArrayList<Field> possibleFields = new ArrayList<>();
+
+        /* Still some cleanup to do
+        if(field instanceof HomeField && card.getValue() == Value.KING || card.getValue() == Value.ACE){
+            Player playerOfField = ((HomeField) field).getPlayer();
+            for (Field key : adjVertices.keySet()){
+                if(key instanceof FirstField && ((FirstField) key).getPlayer()==playerOfField){
+                    ArrayList<Field> fields = new ArrayList<>();
+                    fields.add(key);
+                    return fields;
+                }
+            }
+        }
+        int level = 0;
+        Queue<Field> queue = new LinkedList<>();
+        queue.add(field);
+        queue.add(null);
+        while(!queue.isEmpty() && level<moveValue){
+            Field temp = queue.poll();
+            if(temp==null){
+                level++;
+                queue.add(null);
+            }
+            else{
+                List<Field> adjFields= graph.getAdjFields(temp);
+                if(temp instanceof FirstField && ((FirstField) temp).getBlocked()){
+                    for(Field field1 : adjFields){
+                        if (field1 instanceof GoalField){
+                            adjFields.remove(field1);
+                        }
+                    }
+                }
+                for(Field f: adjFields){
+                    if(f instanceof FirstField && ((FirstField) f).getBlocked()) {
+                        assert true;
+                    }
+                    else if (f instanceof GoalField && ((GoalField) f).getPlayer()!=field.getOccupant().getPlayer()){
+                        assert true;
+                    }
+                    else{
+                        queue.add(f);
+                    }
+                }
+            }
+        }
+        ArrayList<Field> fields = new ArrayList<>();
+        while(!queue.isEmpty()){
+            if(queue.peek()!=null) {
+                fields.add(queue.poll());
+            }
+            else{
+                queue.poll();
+            }
+        }
+         */
+
+        return possibleFields;
     }
 
 }
