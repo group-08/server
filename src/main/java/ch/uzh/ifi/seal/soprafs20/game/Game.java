e9d53c69b2a9dfc240458109902ce8a20b6f2daa
Philip Flury <philip.flury@gmail.com>
2020-04-29 14:02:59 +0200

    rearranged figures in game class

--- src/main/java/ch/uzh/ifi/seal/soprafs20/game/Game.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/game/Game.java
@@ -32,14 +32,6 @@ public class Game implements Serializable {
     @OneToMany(targetEntity = Player.class)
     List<Player> players = new ArrayList<>();
 
-    public Board getBoard() {
-        return board;
-    }
-
-    public void setBoard(Board board) {
-        this.board = board;
-    }
-
     @OneToOne
     Board board;
 
@@ -80,42 +72,12 @@ public class Game implements Serializable {
 
     }
 
-    private boolean canAddPlayer() {
-        return this.players.size() <= 4; // Currently allow for games with 4 players
-    }
-
-    public void addPlayer(Player player) {
-        if (this.canAddPlayer()) {
-            this.players.add(player);
+    public void play(){
+        /*
+        while (game not finished) {
+            this.sixToTwoRound();
         }
-        else {
-            throw new ArrayIndexOutOfBoundsException("Game is already full.");
-        }
-    }
-
-
-    /**
-     * return the next player and keep moving
-     * (I understand this is basically a Queue, but I can't get JPA to accept a Queue)
-     * @return the next player
-     */
-    public Player getNextPlayer(){
-        // Get player on top of the array
-        Player nextPlayer = this.players.get(0);
-
-        // Append the player at the back again
-        this.players.remove(nextPlayer);
-        this.players.add(nextPlayer);
-
-        // Return the player
-        return nextPlayer;
-    }
-
-    private void setPlayer(int id, Player player) {
-        Field field = board.getField(id);
-        Figure figure = field.getFigure();
-        figure.setPlayer(player);
-        player.addFigure(figure);
+         */
     }
 
     public void letPlayersChangeCard() {
@@ -130,7 +92,7 @@ public class Game implements Serializable {
     }
 
     // BigRound is from 6 cards to 2 cards
-    public void handSize() {
+    public void sixToTwoRound() {
         int cardNum = 6;
         while (cardNum >= 2) {
             playRound(cardNum);
@@ -140,11 +102,7 @@ public class Game implements Serializable {
 
     // Round is until no player has a card left
     public void playRound(int cardNum) {
-        for (Player player : players) {
-            List<Card> cards = this.deck.deal(cardNum);
-            player.setHand(cards);
-        }
-
+        this.distributeCards(cardNum);
 
         this.letPlayersChangeCard();
 
@@ -165,27 +123,56 @@ public class Game implements Serializable {
         //Check if won
     }
 
-    public void play(){
-        /*
-        while (game not finished) {
-            this.handSize();
-        }
-         */
+    public Boolean checkIfFinished(Player currentPlayer){
+        return board.checkIfAllTargetFieldsOccupied(id, currentPlayer);
+    }
 
-
-
+    public Boolean checkIfWon(){
+        Player Partner = this.players.get(1);
+        return board.checkIfAllTargetFieldsOccupied(id, Partner);
     }
 
     public void endGame(){
 
     }
 
-    public Boolean checkIfFinished(Player currentPlayer){
-        return board.checkIfAllTargetFieldsOccupied(id, currentPlayer);
-    }
-    public Boolean checkIfWon(){
-        Player Partner = this.players.get(1);
-        return board.checkIfAllTargetFieldsOccupied(id, Partner);
+    public Board getBoard() {
+        return board;
     }
 
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    private boolean canAddPlayer() {
+        return this.players.size() <= 4; // Currently allow for games with 4 players
+    }
+
+    public void addPlayer(Player player) {
+        if (this.canAddPlayer()) {
+            this.players.add(player);
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException("Game is already full.");
+        }
+    }
+
+    public Player getNextPlayer(){
+        // Get player on top of the array
+        Player nextPlayer = this.players.get(0);
+
+        // Append the player at the back again
+        this.players.remove(nextPlayer);
+        this.players.add(nextPlayer);
+
+        // Return the player
+        return nextPlayer;
+    }
+
+    private void setPlayer(int id, Player player) {
+        Field field = board.getField(id);
+        Figure figure = field.getFigure();
+        figure.setPlayer(player);
+        player.addFigure(figure);
+    }
 }
