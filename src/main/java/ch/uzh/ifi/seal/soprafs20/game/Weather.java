525196bf7e7a2b8637c2c6b4fb5cb028aa1b979e
Oli Kamer <oliver.kamer@uzh.ch>
2020-05-17 16:51:28 +0200

    New Weather API

--- src/main/java/ch/uzh/ifi/seal/soprafs20/game/Weather.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/game/Weather.java
@@ -7,29 +7,16 @@ public class Weather {
     HashMap<String, WeatherState> hashMapWeather = new HashMap<String, WeatherState>();
 
     public Weather() {
-        hashMapWeather.put("freezing_rain_heavy", WeatherState.RAINY);
-        hashMapWeather.put("freezing_rain", WeatherState.RAINY);
-        hashMapWeather.put("freezing_rain_light", WeatherState.RAINY);
-        hashMapWeather.put("freezing_drizzle", WeatherState.RAINY);
-        hashMapWeather.put("ice_pellets_heavy", WeatherState.RAINY);
-        hashMapWeather.put("ice_pellets", WeatherState.RAINY);
-        hashMapWeather.put("ice_pellets_light", WeatherState.RAINY);
-        hashMapWeather.put("snow_heavy", WeatherState.RAINY);
-        hashMapWeather.put("snow", WeatherState.RAINY);
-        hashMapWeather.put("snow_light", WeatherState.RAINY);
-        hashMapWeather.put("flurries", WeatherState.WINDY);
-        hashMapWeather.put("tstorm", WeatherState.WINDY);
-        hashMapWeather.put("rain", WeatherState.RAINY);
-        hashMapWeather.put("rain_heavy", WeatherState.RAINY);
-        hashMapWeather.put("rain_light", WeatherState.RAINY);
-        hashMapWeather.put("drizzle", WeatherState.RAINY);
-        hashMapWeather.put("fog_light", WeatherState.SUNNY);
-        hashMapWeather.put("fog", WeatherState.SUNNY);
-        hashMapWeather.put("cloudy", WeatherState.SUNNY);
-        hashMapWeather.put("mostly_cloudy", WeatherState.SUNNY);
-        hashMapWeather.put("partly_cloudy", WeatherState.SUNNY);
-        hashMapWeather.put("mostly_clear", WeatherState.SUNNY);
-        hashMapWeather.put("clear", WeatherState.SUNNY);
+        hashMapWeather.put("Snow", WeatherState.RAINY);
+        hashMapWeather.put("Sleet", WeatherState.RAINY);
+        hashMapWeather.put("Hail", WeatherState.RAINY);
+        hashMapWeather.put("Thunderstorm", WeatherState.WINDY);
+        hashMapWeather.put("Heavy Rain", WeatherState.RAINY);
+        hashMapWeather.put("Light Rain", WeatherState.RAINY);
+        hashMapWeather.put("Showers", WeatherState.RAINY);
+        hashMapWeather.put("Heavy Cloud", WeatherState.WINDY);
+        hashMapWeather.put("Light Cloud", WeatherState.SUNNY);
+        hashMapWeather.put("Clear", WeatherState.SUNNY);
     }
 
     public HashMap<String, WeatherState> getHashMapWeather() {
